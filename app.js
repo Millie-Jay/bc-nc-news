b5d18bb3616688c7e3f586b7a80e4f15c9125d0b
Millie-Jay <millicentbowers@gmail.com>
2024-07-18 08:27:40 +0100

    be able to access comments by id. Combined some testing for get /api/articles

--- app.js
+++ app.js
@@ -1,30 +1,46 @@
-const express = require("express")
-const app = express()
-const {getTopics, getArticleById, getArticles} = require("./controllers/api.topics.controller")
-const endpoints = require("./endpoints.json")
-const db = require("./db/connection")
+const express = require("express");
+const app = express();
+const {
+  getTopics,
+  getArticleById,
+  getArticles,
+  getArticleComments,
+} = require("./controllers/api.topics.controller");
+const endpoints = require("./endpoints.json");
+const db = require("./db/connection");
 
-app.use(express.json())
-
+app.use(express.json());
+//adds body to the request keys.
+//I will remove this comment on later tickets, I just want to see it so it sinks in.
 
 app.get("/api", (request, response, next) => {
-  response.status(200).send({endpoints: endpoints})
-})
+  response.status(200).send({ endpoints: endpoints });
+});
 
-app.get("/api/topics", getTopics)
+app.get("/api/topics", getTopics);
 
-app.get("/api/articles/:article_id", getArticleById)
+app.get("/api/articles/:article_id", getArticleById);
 
-app.get("/api/articles", getArticles)
+app.get("/api/articles", getArticles);
 
-
+app.get("/api/articles/:article_id/comments", getArticleComments);
 
 app.use((err, request, response, next) => {
-  console.log(err, "ERROR")
-  if(err.status && err.message) {
-    response.status(err.status).send( {message: err.message })
-  }
-    next(err);
-})
+  console.log(err);
+  if (err.status) {
+    response.status(err.status).send({ msg: err.msg });
+  } else next(err);
+});
+
+app.use((err, request, response, next) => {
+  if (err.code === "22P02") {
+    response.status(400).send({ msg: "400: Bad request" });
+  } else next(err);
+});
+
+app.use((err, request, response, next) => {
+  console.log(err);
+  response.status(500).send({ msg: "Internal Server Error" });
+});
 
 module.exports = app;
